b6c0226fcc6ef9dabae680e2a8e2301f473cfd66
Brandon Coroy <becgonzalez25@gmail.com>
2022-05-13 14:17:43 -0600

    mostrando los servicios en las tarjetas

--- src/app/models/destino-viaje.model.ts
+++ src/app/models/destino-viaje.model.ts
@@ -1,12 +1,15 @@
 export class DestinoViaje{
 
   private selected: boolean = false;
+  public servicios:string[];
 
   constructor(
     public titulo:string,
     public subtitulo:string,
     public imgUrl:string
-  ) {};
+  ) {
+    this.servicios=['desayuno', 'almuerzo', 'cena']
+  };
 
   isSelected(): boolean {
     return this.selected;
