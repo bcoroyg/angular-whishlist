39e9e3cbf8454c75ba2aed71f9e8e55646b0cbff
Brandon Coroy <becgonzalez25@gmail.com>
2022-05-13 11:13:55 -0600

    Lista de destinos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './components/destino-viaje/destino-viaje.component';
+import { ListaDestinosComponent } from './components/lista-destinos/lista-destinos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DestinoViajeComponent
+    DestinoViajeComponent,
+    ListaDestinosComponent
   ],
   imports: [
     BrowserModule,
