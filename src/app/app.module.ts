c903b81bd32eca86bb465fb5a1fe555b4058c07b
Brandon Coroy <becgonzalez25@gmail.com>
2022-05-13 10:38:23 -0600

    Agregando bootstrap y creando componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DestinoViajeComponent } from './components/destino-viaje/destino-viaje.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DestinoViajeComponent
   ],
   imports: [
     BrowserModule,
