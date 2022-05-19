924bd456d1549003c10b026b8c43ab37d0d81b0e
Brandon Coroy <becgonzalez25@gmail.com>
2022-05-19 11:51:10 -0600

    Validaciones personalizadas

--- src/app/components/form-destino-viaje/form-destino-viaje.component.ts
+++ src/app/components/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { DestinoViaje } from 'src/app/models/destino-viaje.model';
 
 @Component({
@@ -10,17 +10,24 @@ import { DestinoViaje } from 'src/app/models/destino-viaje.model';
 export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
   fg:FormGroup;
+
+  minLongSub:number=3;
+
   constructor(fb:FormBuilder) {
     this.onItemAdded=new EventEmitter();
     this.fg = fb.group({
-      titulo:['', Validators.required],
-      subtitulo:['', Validators.required],
+      titulo:['', Validators.compose([Validators.required, this.tituloValidator])],
+      subtitulo:['', [Validators.required, this.subtituloValidatorParametrizable(this.minLongSub)]],
       imagen:['', Validators.required]
     });
 
-    this.fg.valueChanges.subscribe((form:any)=> {
+/*     this.fg.valueChanges.subscribe((form:any)=> {
       console.log("Cambio Form -->",form)
-    })
+    });
+
+    this.fg.controls['titulo'].valueChanges.subscribe((value: string) => {
+      console.log('titulo cambio: ', value);
+    }); */
   }
 
   ngOnInit(): void {
@@ -30,6 +37,24 @@ export class FormDestinoViajeComponent implements OnInit {
     const destino = new DestinoViaje(titulo,subtitulo,urlImg)
     this.onItemAdded.emit(destino);
     return false
+  };
+
+  tituloValidator(control: FormControl): { [keyValid: string]: boolean } {
+    const longitud = control.value.toString().trim().length;
+    if (longitud > 0 && longitud < 5) {
+      return { invalidTitulo: true };
+    };
+    return {};
+  }
+
+  subtituloValidatorParametrizable(minLong: number): ValidatorFn {
+    return (control: AbstractControl): { [keyValid: string]: boolean } | null => {
+      const longitud = control.value.toString().trim().length;
+      if (longitud > 0 && longitud < minLong) {
+        return { minLongSubtitulo: true };
+      }
+      return null;
+    };
   }
 
 }
