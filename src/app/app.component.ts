f9a6966e905a49ad66eeb5a1d91d57b48a28d829
Brandon Coroy <becgonzalez25@gmail.com>
2022-05-19 12:24:21 -0600

    Agregando la fecha y hora con Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'whishlist';
+  time = new Observable(observer => {
+    setInterval(()=> {
+      return observer.next(new Date().toString())
+    }, 1000);
+  })
 }
